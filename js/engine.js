48327a947ffffcba55a0473838fd05571cc8cf38
intheon <allobon@gmail.com>
2014-12-22 11:22:28 +0000

    gets credentials

--- js/engine.js
+++ js/engine.js
@@ -2,6 +2,8 @@
 
 $(document).ready(function(){
 
+	// the controller for the main panels show/hide
+
 	$(".one-third a").click(function(event){
 
 			// get the link target
@@ -12,6 +14,7 @@ $(document).ready(function(){
 			// hide all the others
  
 		$(".two-thirds").each(function(event){
+			$(".message").hide();
 			$(this).hide();
 		});
 
@@ -19,6 +22,56 @@ $(document).ready(function(){
 
 		$("." + target).hide().fadeIn();
 
-	})
+	});
+
+	checkLogIn();
+
+	// the controller to get admin keys
+
+	$("#submit_admin").click(function(){
+		var key = $("#key").val();
+		var shared = $("#shared").val();
+
+		if (!key || !shared)
+		{
+			$(".message").append("<div class='warning'>These cannot be empty X</div>")
+			attachListeners();
+		}
+		else
+		{
+			localStorage.setItem("adminKey",key);
+			localStorage.setItem("adminShared",shared);	
+			checkLogIn();
+		}
+	});
 
 });
+
+function attachListeners(){
+	$(".warning").click(function(){
+		$(this).fadeOut(function(){
+			$(this).hide();
+		});
+	});
+};
+
+
+function checkLogIn()
+{
+	var adminKey = localStorage.getItem("adminKey");
+	var adminShared = localStorage.getItem("adminShared");
+
+	if (!adminKey || !adminShared)
+	{
+		$("#signed_out").fadeIn();
+	}
+	else 
+	{
+		$("#signed_out").fadeOut();
+		$("#signed_in").fadeIn();
+
+		$("#api_output").append(adminKey);
+		$("#shared_output").append(adminShared);
+	}
+
+}
